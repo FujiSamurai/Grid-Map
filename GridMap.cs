f3fd7fec378a2dfb0b423b2ed16116a9ea69b3fa
FujiSamurai <kunterkurt0@gmail.com>
2023-12-13 17:10:33 +0300

    Update GridMap.cs

--- GridMap.cs
+++ GridMap.cs
@@ -1,26 +1,128 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using System;
 
-public class GridMap : MonoBehaviour
+public class GridMap
 {
-    [SerializeField] private GameObject prefab;
+    public const int sortingOrderDefault = 5000;
 
-    private int rightPosX = -3;
-    private int rightPosY = 0;
-    private int leftPosX = 3;
-    private int leftPosY = 0;
+    private int height;
+    private int width;
+    private float cellSize;
+    private Vector3 originPosition;
+    private int[,] gridArray;
+    private TextMesh[,] debugTextArray;
 
-    private void Start()
+    public static TextMesh CreateWorldText(string text, Transform parent = null, Vector3 localPosition = default(Vector3), int fontSize = 40, Color? color = null, TextAnchor textAnchor = TextAnchor.UpperLeft, TextAlignment textAlignment = TextAlignment.Left, int sortingOrder = sortingOrderDefault)
     {
-        Quaternion rotationForCubeRight = Quaternion.Euler(0f, 0f, 45f);
-        Quaternion rotationForCubeLeft = Quaternion.Euler(0f, 0f, 30f);
+        if (color == null) color = Color.white;
+        return CreateWorldText(parent, text, localPosition, fontSize, (Color)color, textAnchor, textAlignment, sortingOrder);
+    }
 
-        GameObject cubeRight = Instantiate(prefab, new Vector3(rightPosX, rightPosY), rotationForCubeRight);
+    public static TextMesh CreateWorldText(Transform parent, string text, Vector3 localPosition, int fontSize, Color color, TextAnchor textAnchor, TextAlignment textAlignment, int sortingOrder)
+    {
+        GameObject gameObject = new GameObject("World_Text", typeof(TextMesh));
+        Transform transform = gameObject.transform;
+        transform.SetParent(parent, false);
+        transform.localPosition = localPosition;
+        TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+        textMesh.anchor = textAnchor;
+        textMesh.alignment = textAlignment;
+        textMesh.color = color;
+        textMesh.fontSize = fontSize;
+        textMesh.text = text;
+        textMesh.GetComponent<MeshRenderer>().sortingOrder = sortingOrder;
+        return textMesh;
+    }
 
-        GameObject cubeLeft = Instantiate(prefab, new Vector3(leftPosX, leftPosY), rotationForCubeLeft);
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vector = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vector.z = 0;
+        return vector;
+    }
+    public static Vector3 GetMouseWorldPositionWithZ()
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
 
-        cubeRight.transform.localScale = new Vector3(3, 3, 3);
-        cubeLeft.transform.localScale = new Vector3(2, 2, 2);
+    public GridMap(int width, int height, float cellSize, Vector3 originPoisition)
+    {
+        this.height = height;
+        this.width = width;
+        this.cellSize = cellSize;
+        this.originPosition = originPoisition;
+
+        gridArray = new int[width, height];
+        debugTextArray = new TextMesh[width, height];
+
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * .5f, 20, Color.white, TextAnchor.MiddleCenter);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+            }
+        }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+    }
+
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize + originPosition;
+    }
+
+    private void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt(worldPosition.x / cellSize);
+        y = Mathf.FloorToInt(worldPosition.y / cellSize);
+    }
+
+    public void SetValue(int x, int y, int value)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            gridArray[x, y] = value;
+            debugTextArray[x, y].text = gridArray[x, y].ToString();
+        }
+    }
+
+    public void SetValue(Vector3 worldPosition, int value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
+    }
+
+    public int GetValue(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            return gridArray[x, y];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public int GetValue(Vector3 worldPosition)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetValue(x, y);
     }
 }
